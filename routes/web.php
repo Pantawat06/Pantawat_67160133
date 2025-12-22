2125fda61e9ed3ec7dc2e8d33ccb4f7122454619
Pantawat06 <67160133@go.buu.ac.th>
2025-12-22 17:44:37 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use App\Http\Controllers\MyController;
 
 Route::get('/', [MyController::class, 'index'])->name('workshop.index');
 Route::post('/submit', [MyController::class, 'store'])->name('workshop.store');
+
+Route::namespace('App\Http\Controllers')->group(function(){
+    Route::get('/flights', 'FlightController@index');
+});
